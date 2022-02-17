83f9a812b6b3c552f1e5978992925de2e3c5384e
Johann Kergal <89657288+jkergal@users.noreply.github.com>
2022-02-17 11:58:18 +0100

    Update backupOSX.sh

--- backupOSX.sh
+++ backupOSX.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 date=$(date +%Y-%m-%d-%H-%M-%S)
-rsync -a . --exclude "/node_modules" "/Volumes/GoogleDrive/Mon Drive/dev-cloud/dev-backup/"$date"__OSX"
+rsync -a . --exclude "/node_modules" "/Volumes/your/path/destination/"$date"__OSX"
