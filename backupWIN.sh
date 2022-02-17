0f5708052d83013c6accd55c683b3ba0c11bd8db
Johann Kergal <89657288+jkergal@users.noreply.github.com>
2022-02-17 11:59:35 +0100

    Update backupWIN.sh

--- backupWIN.sh
+++ backupWIN.sh
@@ -6,7 +6,7 @@ date=$(date +%Y-%m-%d-%H-%M-%S)
 
 folderDestination="$currentFolder$date"
 
-rsync -a . --exclude "/node_modules" "/mnt/g/Mon Drive/dev-cloud/dev-backup/$currentFolder$date __WIN"
+rsync -a . --exclude "/node_modules" "/mnt/g/your/path/destination/$currentFolder$date __WIN"
 
 
 
